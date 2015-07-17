9bd91e5e2ab0b5130df45fbb6fabc8c587c3ae32
Volodymyr Kopytin <volodymyr.kopytin@rebelmouse.com>
2015-07-18 00:38:31 +0200

    amd config module path update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 require.config({
+  baseUrl: 'js/',
   paths: {
-    text: '/js/libs/text',
-    jquery: '/js/libs/jquery-1.11.2',
-    hogan: '/js/libs/hogan-3.0.2.amd',
-    underscore: '/js/libs/underscore',
-    backbone: '/js/libs/backbone',
-    localStorage: '/js/libs/backbone.localStorage'
+    text: 'libs/text',
+    jquery: 'libs/jquery-1.11.2',
+    hogan: 'libs/hogan-3.0.2.amd',
+    underscore: 'libs/underscore',
+    backbone: 'libs/backbone',
+    localStorage: 'libs/backbone.localStorage'
   },
   shim: {
 	  localStorage: {
