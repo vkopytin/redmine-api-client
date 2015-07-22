f6865686fa3d1766abd37810bcf3f9ff8abe32fe
Volodymyr Kopytin <volodymyr.kopytin@rebelmouse.com>
2015-07-22 22:02:45 +0200

    cleaning project

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// File: config.php
-// Main configuration
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-$text = "Hello %s!";
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// File: index.php
-// Main program.
-require_once "config.php";
-$name = $_GET("name");
-echo printf($text, $name);
