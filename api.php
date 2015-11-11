40767c0d2c8053832090de127b7d88a2367e12b6
Volodymyr Kopytin <volodymyr.kopytin@rebelmouse.com>
2015-11-11 12:49:31 +0100

    code cleanup

--- api.php
+++ api.php
@@ -4,29 +4,6 @@ require_once 'Client.php';
 class API {
     private static $url = 'https://redmine.rebelmouse.com';
 
-    public static function run1 () {
-        $httpHeader = array();
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_VERBOSE, 0);
-        curl_setopt($curl, CURLOPT_HEADER, 0);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_USERNAME, 'che');
-        curl_setopt($curl, CURLOPT_PASSWORD, 'guevara2012!');
-        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_URL, API::$url . '/issues.json');
-        curl_setopt($curl, CURLOPT_PORT, 443);
-        $httpHeader[] = 'X-Redmine-API-Key: '.API::$key;
-        $httpHeader[] = 'Expect: ';
-        $httpHeader[] = 'Content-Type: application/json';
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $httpHeader);
-        $response = trim(curl_exec($curl));
-        echo curl_error($curl);
-        curl_close($curl);
-        echo $response;
-    }
-
     private static function getAuth() {
         $path = $_SERVER['PATH_INFO'];
         $re = "/\\/(?P<user>[\\w]+)[:]*(?P<password>.*)[:](?P<key>[\\w\\n]+)\\/(?P<path>.*)/";
